2faf272cc28801cef10adb88689e3e97e2f41ed8
Terraform User <terraform@example.com>
2025-06-14 19:29:38 -0300

    Add variables file

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "hvn_id" {
+  description = "ID of the HVN"
+  type        = string
+}
+
+variable "hvn_cloud_provider" {
+  description = "Cloud provider for the HVN"
+  type        = string
+}
+
+variable "hvn_region" {
+  description = "Region for the HVN"
+  type        = string
+}
+
+variable "hvn_cidr_block" {
+  description = "CIDR block for the HVN"
+  type        = string
+}
